a2c355c29691ede605023059eec24990a83e789d
Mr-Macko <max.allesch@gmx.de>
2022-03-16 00:04:05 +0100

    implemented endpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
